851857630e3ff13323df9472d5cbf196b5c90dd7
cameyer260 <124843470+cameyer260@users.noreply.github.com>
2024-03-12 11:38:53 -0500

    Add files via upload

--- Program18.java
+++ Program18.java
@@ -1,6 +1,7 @@
 package program18;
 
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Program18 {
 
@@ -65,77 +66,135 @@ public class Program18 {
 	}
 	
 	/**
-	 * the parameters are 4 x and y coordinates of the or x, y, x, y ...
-	 * the first 3 xy pairs are the vertices of a triangle
-	 * the method returns true if the 4th xy pair is in the triangle(or on the border)
-	 * @param p1x
-	 * @param p1y
-	 * @param p2x
-	 * @param p2y
-	 * @param p3x
-	 * @param p3y
-	 * @param p4x
-	 * @param p4y
+	 * returns true if point target is within the triangle made by triangle points
+	 * @param trianglePoints
+	 * @param target
 	 * @return
 	 */
-	private static boolean function3(double p1x, double p1y, double p2x, double p2y, double p3x, double p3y, double p4x, double p4y) {
-		ArrayList<Double> orderedByX = sort(p1x, p1y, p2x, p2y, p3x, p3y);
-		ArrayList<Double> orderedByY = sort(p1y, p1x, p2y, p2x, p3y, p3x);
-		double right, left;
-		double up, down;
-		double m, b;
-		//case 1 - no vertices of the triangle have the same x or y values
-		if(((orderedByX.get(0) < orderedByX.get(2)) && (orderedByX.get(2) < orderedByX.get(4))) &&
-			((orderedByY.get(0) < orderedByY.get(2)) && (orderedByY.get(2)) < orderedByY.get(4))) {
-			m = slopeOf(orderedByX.get(2), orderedByX.get(3), orderedByX.get(4), orderedByX.get(5));
-			b = orderedByX.get(5) - m*orderedByX.get(4);
-			right = (p4y-b)/m;
-			m = slopeOf(orderedByX.get(2), orderedByX.get(3), orderedByX.get(0), orderedByX.get(1));
-			b = orderedByX.get(1) - m*orderedByX.get(0);
-			left = (p4y-b)/m;
-			// now find what up and down would be
-			
-			//then check if left<=p4x<=right && down<=p4x<=up
+	private static boolean function3(double[][] trianglePoints, double[] target) {
+		double[][] polygonPoints = new double[4][2];
+		for(int i=0; i<3; i++) {
+			polygonPoints[i][0] = trianglePoints[i][0];
+			polygonPoints[i][1] = trianglePoints[i][1];
+		}
+		polygonPoints[3][0] = target[0];
+		polygonPoints[3][1] = target[1];
+		
+		/**
+		 * if the triangle has a larger area the the polygon made by the triangle points AND
+		 * the target point, then the point is in the triangle, if not, the point is outside the
+		 * triangle (polygon is bigger than triangle)
+		 */
+		if(calculateTriangleArea(trianglePoints) > calculateQuadrilateralArea(polygonPoints)) {
+			return true;
+		} else return false;
+	}
+	
+	private static double distanceFormula(double[] point1, double[] point2) {
+		return Math.sqrt(Math.pow(point2[0] - point1[0], 2) + Math.pow(point2[1] - point2[1], 2));
+	}
+	
+	private static void sortPointsByX(double[][] points) {
+		
+	}
+	
+	private static double calculateTriangleArea(double[][] points) {
+		double a = distanceFormula(points[0], points[1]);
+		double b = distanceFormula(points[1], points[2]);
+		double c = distanceFormula(points[2], points[3]);
+		
+		double semiP = (a + b + c)/2;
+		return Math.sqrt(semiP*(a-semiP)*(b-semiP)*(c-semiP));
+	}
+	
+	/**
+	 * returns the area of the polgyon made by the points passed through the parameter
+	 * @param points
+	 * @return
+	 */
+	private static double calculateQuadrilateralArea(double[][] points) {
+		if(points.length < 3) {
+			throw new IllegalArgumentException("A polygon must have at least 3 vertices");
 		}
 		
-		return false;
+		double total = 0;
+		for(int i=0; i< points.length; i++) {
+			int j = (i+1) % points.length;
+			total+= points[i][0] * points[j][1] - points[j][0] * points[i][1];
+		}
+		return Math.abs(total / 2.0);
 	}
 	
-	private static double slopeOf(double x1, double y1, double x2, double y2) {
-		return (y2-y1)/(x2-x1);
-	}
-	
-	private static ArrayList<Double> sort(double a, double a2, double b, double b2, double c, double c2) {
-		ArrayList<Double> array = new ArrayList<Double>();
-		if(a>b) {
-			array.add(b);
-			array.add(b2);
-			array.add(a);
-			array.add(a2);
-		} else {
-			array.add(a);
-			array.add(a2);
-			array.add(b);
-			array.add(b2);
-		}
-		if(c<array.get(0)) {
-			array.add(1, c);
-			array.add(2, c2);
-		} else if(c<array.get(2)) {
-			array.add(2, c);
-			array.add(3, c2);
-		} else {
-			array.add(c);
-			array.add(c2);
-		}
-		return array;
-	}
 	/**
 	 * main method
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		try {
+			FileWriter myWriter = new FileWriter("recursiveBinaryTriangle.txt");
+			myWriter.write("Function 1 Output: \n");
+			myWriter.write("f(1): " + function1(1) + "\n");
+			myWriter.write("f(15): " + function1(15) + "\n");
+			myWriter.write("f(27): " + function1(27) + "\n");
+			myWriter.write("f(53): " + function1(53) + "\n");
+			myWriter.write("\n");
+			myWriter.write("Function 2: \n");
+			myWriter.write("Using the following points: x=1, y=1000, z=1023 + \n");
+			myWriter.write("Output: " + function2(1, 1000, 1023) + "\n");
+			myWriter.write("\n");
+			myWriter.write("Function 3: \n");
+			double[][] tPoints = new double[3][2];
+			tPoints[0][0]=2.2;
+			tPoints[0][1]=5.6;
+			tPoints[1][0]=1.1;
+			tPoints[1][1]=2.2;
+			tPoints[2][0]=2.4;
+			tPoints[2][1]=4.7;
+			double[] target = new double[2];
+			target[0] = 2.5;
+			target[1] = 3.0;
+			if(function3(tPoints, target)) {
+				myWriter.write("The point (" + target[0] + ", " + target[1] + ") is "
+						+ "inside the triangle with coordinates (" + tPoints[0][0] + ", " 
+						+ tPoints[0][1] + "), " + "(" + tPoints[1][0] + ", " 
+						+ tPoints[1][1] + "), " + "(" + tPoints[2][0] + ", " 
+						+ tPoints[2][1] + ").");
+			} else {
+				myWriter.write("The point (" + target[0] + ", " + target[1] + ") is "
+						+ "not inside the triangle with coordinates (" + tPoints[0][0] + ", " 
+						+ tPoints[0][1] + "), " + "(" + tPoints[1][0] + ", " 
+						+ tPoints[1][1] + "), " + "(" + tPoints[2][0] + ", " 
+						+ tPoints[2][1] + ").");
+			}
+			myWriter.write('\n');
+			
+			double[][] t2Points = new double[3][2];
+			t2Points[0][0]=-4.2;
+			t2Points[0][1]=3.0;
+			t2Points[1][0]=-2.7;
+			t2Points[1][1]=3.0;
+			t2Points[2][0]=-9.1;
+			t2Points[2][1]=8.18;
+			double[] target2 = new double[2];
+			target2[0] = -4.4;
+			target2[1] = 3.8;
+			if(function3(t2Points, target2)) {
+				myWriter.write("The point (" + target2[0] + ", " + target2[1] + ") is "
+						+ "inside the triangle with coordinates (" + tPoints[0][0] + ", " 
+						+ t2Points[0][1] + "), " + "(" + t2Points[1][0] + ", " 
+						+ t2Points[1][1] + "), " + "(" + t2Points[2][0] + ", " 
+						+ t2Points[2][1] + ").");
+			} else {
+				myWriter.write("The point (" + target2[0] + ", " + target2[1] + ") is "
+						+ "not inside the triangle with coordinates (" + tPoints[0][0] + ", " 
+						+ t2Points[0][1] + "), " + "(" + t2Points[1][0] + ", " 
+						+ t2Points[1][1] + "), " + "(" + t2Points[2][0] + ", " 
+						+ t2Points[2][1] + ").");
+			}
+			myWriter.close();
+		} catch (IOException e) {
+			System.out.println("IOException");
+			e.printStackTrace();
+		}	
 	}
-
 }
